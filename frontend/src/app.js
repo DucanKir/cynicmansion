2139d4963ae9121cb44bcbef4002e716fd9259a7
Lana Kirilenko <svetlana.kirilenko@hotmail.com>
2019-10-15 15:28:53 +0100

    initial setup

---
+++ frontend/src/app.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/free-solid-svg-icons'
+import '@fortawesome/react-fontawesome'
+
+import Game from './game/Game'
+
+import 'bulma'
+import './style.scss'
+
+class App extends React.Component {
+
+  render(){
+    return (
+      <div>
+        <h1>hi</h1>
+        <Game />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ frontend/src/game/Game.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import axios from 'axios'
+import Promise from 'bluebird'
+
+class Game extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+    }
+  }
+
+  render() {
+
+    return (
+      <section className="section">
+        <h1>Hello</h1>
+      </section>
+    )
+  }
+}
+
+export default Game
