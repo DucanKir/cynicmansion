9dafe34184a11de7466d92eb7f78a586b1f5fdd1
Lana Kirilenko <lana.kirilenko@vida.co.uk>
2020-08-27 23:05:07 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="cyniceditor", # Replace with your own username
+    version="0.0.1",
+    author="ducankir",
+    author_email="author@example.com",
+    description="A small example package",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
